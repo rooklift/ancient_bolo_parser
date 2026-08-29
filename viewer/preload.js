c41f6c73859d92b967d296e2727bdfc94eb2d478
rooklift <16438795+rooklift@users.noreply.github.com>
2026-08-29 22:03:18 +0100

    Minimal change to support ancient Electron

--- viewer/preload.js
+++ viewer/preload.js
@@ -1,10 +1,15 @@
 "use strict";
+
 const { contextBridge, ipcRenderer, webUtils } = require("electron");
 
 contextBridge.exposeInMainWorld("api", {
 	open_log: () => ipcRenderer.invoke("open-log"),
 	save_map: (name, data) => ipcRenderer.invoke("save-map", name, data),
-	file_path: file => webUtils.getPathForFile(file),
+
+	file_path: file => webUtils
+			? webUtils.getPathForFile(file)
+			: file.path,
+
 	file_loaded: file_path => ipcRenderer.send("file-loaded", file_path),
 	show_file: () => ipcRenderer.send("show-file"),
 	exit_fullscreen: () => ipcRenderer.send("exit-fullscreen"),
